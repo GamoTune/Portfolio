5aaecc9edd42b8eb3febe8425cc288c60b3112b9
Patrick <38393544+128kPatrick@users.noreply.github.com>
2024-02-24 18:24:29 +0100

    ouverture projet

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    console.log("main.js ready!");
+
+    $(".projet_details").hide();
+    $("#projet_bouton_1").on( "click", function() {
+        $(".div_portfolio .projets").hide("slow");
+        $("#projet_details_1").show("slow");
+    });
+    $(".bt_retour").on( "click", function() {
+        $(".projet_details").hide("slow");
+        $(".div_portfolio .projets").show("slow");
+    });
+
     const temps_transition = 500;
     $("#bt_haut").on( "click", function() {
         $('html, body').animate({ scrollTop: $("#haut").offset().top }, temps_transition);
@@ -12,4 +24,5 @@ $(document).ready(function(){
     $("#bt_contact").on( "click", function() {
         $('html, body').animate({ scrollTop: $("#contact").offset().top }, temps_transition);
     });
+
 });
