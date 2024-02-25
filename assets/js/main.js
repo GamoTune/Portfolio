6f9ae3d276217523921cd8158623a02f01940c0e
Patrick <38393544+128kPatrick@users.noreply.github.com>
2024-02-25 16:40:22 +0100

    mise à jour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
     console.log("main.js ready!");
-
+    const temps_transition = 500;
     $(".projet_details").hide();
 
     $(".projet_bouton").on( "click", function() {
         num = $(this).attr("id").split("_")[2];
         console.log($(this).attr("id"));
         $(".div_portfolio .projets").hide("slow");
-        $("#projet_details_"+num).show("slow");
+        $("#projet_details_"+num).show("slow",function(){
+            $('html, body').animate({ scrollTop: $("#projet_details_"+num).offset().top-50 }, temps_transition);
+        }); 
     });
 
     $(".bt_retour").on( "click", function() {
+        $('html, body').animate({ scrollTop: $("#portfolio").offset().top+50 }, temps_transition);
         $(".projet_details").hide("slow");
         $(".div_portfolio .projets").show("slow");
     });
 
     //-------------------------------------------- boutons menu navigation
-    const temps_transition = 500;
+    
     $(".div_menu a").on( "click", function() {
         bt=$(this).attr("id").split("_")[1];
         $('html, body').animate({ scrollTop: $("#"+bt).offset().top }, temps_transition);
